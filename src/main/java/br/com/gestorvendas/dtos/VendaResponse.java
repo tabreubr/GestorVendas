93607fac616949b29b8c3e5002695fcfbe300669
Thiago Abreu <tabreu.br@gmail.com>
2025-10-12 22:14:52 -0300

    ItemVendaResponse incluído no VendaResponse Dto, correção em UseCase ListarVendas e o CorsConfig

--- src/main/java/br/com/gestorvendas/dtos/VendaResponse.java
+++ src/main/java/br/com/gestorvendas/dtos/VendaResponse.java
@@ -7,6 +7,7 @@ import br.com.gestorvendas.model.VendaEntity;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Builder
 @AllArgsConstructor
@@ -17,11 +18,21 @@ public class VendaResponse {
 
     private Integer id;
     private LocalDateTime dataHora;
+    private List<ItemVendaResponse> itens;
+    private Double total;
 
     public static VendaResponse of(VendaEntity entity) {
         return VendaResponse.builder()
                 .id(entity.getId())
                 .dataHora(entity.getDataHora())
                 .build();
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class ItemVendaResponse {
+        private ProdutoResponse produto;
+        private Integer quantidade;
     }
 }
--- src/main/java/br/com/gestorvendas/usecases/venda/ListarVendas.java
+++ src/main/java/br/com/gestorvendas/usecases/venda/ListarVendas.java
@@ -1,5 +1,6 @@
 package br.com.gestorvendas.usecases.venda;
 
+import br.com.gestorvendas.dtos.ProdutoResponse;
 import br.com.gestorvendas.dtos.VendaRequest;
 import br.com.gestorvendas.dtos.VendaResponse;
 import br.com.gestorvendas.exceptions.ProdutoNaoEncontrado;
@@ -15,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -22,8 +24,27 @@ public class ListarVendas {
 
     private final VendaRepository vendaRepository;
 
-
     public List<VendaResponse> execute() {
-        return vendaRepository.findAll().stream().map(VendaResponse::of).toList();
+        return vendaRepository.findAll().stream().map(v -> {
+            var itens = v.getItens().stream().map(i ->
+                    new VendaResponse.ItemVendaResponse(
+                            new ProdutoResponse(
+                                    i.getProduto().getId(),
+                                    i.getProduto().getNome(),
+                                    i.getProduto().getDescricao(),
+                                    i.getProduto().getQuantidade(),
+                                    i.getProduto().getPreco()
+                            ),
+                            i.getQuantidade()
+                    )
+            ).collect(Collectors.toList());
+
+            double total = itens.stream()
+                    .mapToDouble(i -> i.getProduto().getPreco() * i.getQuantidade())
+                    .sum();
+
+            return new VendaResponse(v.getId(), v.getDataHora(), itens, total);
+        }).collect(Collectors.toList());
     }
+
 }
